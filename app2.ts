3f99731ed93acb9292467012be0325bfbd167b42
Crypt06545 <mehedihasansagor2005@gmail.com>
2025-06-01 15:45:35 +0600

    basic Type Inferance

---
+++ app2.ts
@@ -0,0 +1,12 @@
+/**
+ * Type Inferance
+ * understanding type inferance
+ * Type annotations
+ */
+
+//  * Type Inferance ( if we do not mention variable the data type & typescript auto detect it)
+let a = "12";
+
+//  * Type annotations ( when we mention the variable data type )
+let b: number = 23;
+
